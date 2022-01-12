af76fcc70aa50f587e619d0a72428d74de0ff59c
ntan1902 <trinh.an.1902@gmail.com>
2022-01-12 09:30:42 +0700

    init rule-engine

---
+++ rule-engine/application/src/main/java/com/iot/server/application/rabbitmq/RabbitMQConsumerTemplate.java
@@ -0,0 +1,32 @@
+package com.iot.server.application.rabbitmq;
+
+import com.google.gson.reflect.TypeToken;
+import com.iot.server.common.model.TelemetryMsg;
+import com.iot.server.common.utils.GsonUtils;
+import com.iot.server.domain.ts.TsKvService;
+import com.iot.server.queue.message.QueueMsg;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CompletableFuture;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class RabbitMQConsumerTemplate {
+
+    private final TsKvService tsKvService;
+
+    @RabbitListener(queues = "${queue.rabbitmq.telemetry.queue-name}")
+    public void postTelemetry(String msg) {
+        QueueMsg<TelemetryMsg> queueMsg =
+                GsonUtils.fromJson(msg, new TypeToken<QueueMsg<TelemetryMsg>>() {
+                }.getType());
+        log.trace("Consume message {}", queueMsg);
+
+        CompletableFuture
+                .runAsync(() -> tsKvService.saveOrUpdate(queueMsg));
+    }
+}
--- transport/application/src/main/java/com/iot/server/application/controller/mqtt/MqttController.java
+++ transport/application/src/main/java/com/iot/server/application/controller/mqtt/MqttController.java
@@ -12,7 +12,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.Charset;
-
 @Slf4j
 @RequiredArgsConstructor
 @Component
